72e2b182f1ee9a63c1d33e688b858abc961b0d40
Alexey Zamasskin <zamasskin@yandex.ru>
2021-11-09 18:43:07 +0300

    install app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const tracking = require("./tracking");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
